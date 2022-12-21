393b84e69db285d366b71264bf73c72747f6d581
SayedKhaledd <63265214+SayedKhaledd@users.noreply.github.com>
2022-12-21 18:16:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-require("dotenv").config()
-const app = require('./app/app')
+
+//const app = require('./app/app')
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.get('/', (req, res) => {
     res.send('hello world')
 })
