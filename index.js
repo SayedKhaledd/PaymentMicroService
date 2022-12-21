a1a96d58d9cac04acd5dea8f8fdede2f10c9ae3e
SayedKhaledd <63265214+SayedKhaledd@users.noreply.github.com>
2022-12-21 18:13:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 require("dotenv").config()
 const app = require('./app/app')
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
 app.listen(process.env.PORT||3000, () => console.log("server"))
