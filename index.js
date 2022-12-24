bc210aaf4a5caabc26b535ecc2e5a568a72240b7
SayedKhaledd <63265214+SayedKhaledd@users.noreply.github.com>
2022-12-24 05:26:04 +0200

    trail

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 
-const app = require('./app/app')
+/*const app = require('./app/app')
 
 
 
 
-app.listen(process.env.PORT||3000, () => console.log("server"))
+app.listen(process.env.PORT||3000, () => console.log("server"))*/
+
+let express = require('express')
+var bodyParser = require('body-parser')
+
+const app = express()
+//setting our view engine to ejs
+
+app.set('view engine','ejs')
+
+app.get('/', (req, res)=>{
+ res.render('index')
+})
